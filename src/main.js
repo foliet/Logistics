7cc521e5ad4c12564ff27eed0e53d97c06761061
Foliet <foliet@rainspace.cn>
2021-12-26 02:04:34 +0800

    增加了echarts模组用来做数据可视化

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router/index'
 import axios from 'axios'
+import * as echarts from 'echarts'
 
 const app = createApp(App)
 app.config.unwrapInjectedRef = true
@@ -11,4 +12,5 @@ app.use(router)
 app.use(ElementPlus)
 axios.defaults.withCredentials = true;
 app.config.globalProperties.$axios = axios
+app.config.globalProperties.$echarts = echarts
 app.mount('#app')
--- src/view/admin/home.vue
+++ src/view/admin/home.vue
@@ -1,12 +1,58 @@
 <template>
-<div></div>
+<div id="main" style="height: 100%;width: 100%"></div>
 </template>
 
 <script>
 export default {
   mounted() {
     document.title="站点统计"
-  }
+    const chart = this.$echarts.init(document.getElementById("main"))
+    const option = {
+      title: {
+        text: '趋势'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['新增用户数', '新增订单数']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      toolbox: {
+        feature: {
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: '新增用户数',
+          type: 'line',
+          stack: 'Total',
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: '新增订单数',
+          type: 'line',
+          stack: 'Total',
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+      ]
+    };
+    chart.setOption(option)
+  },
 }
 </script>
 
