4302cc6a3b33abec2a69b5f8863bf0e455d70aec
fengzihan <2753089253@qq.com>
2021-12-27 00:57:51 +0800

    加了界面，但是好像不太完美

--- src/view/admin/home.vue
+++ src/view/admin/home.vue
@@ -1,9 +1,42 @@
 <template>
-  <div id="main" style="height: 70%;width: 60%"></div>
+  <span id="main" class="picture"></span>
+  <span id="number">
+    <el-card :body-style="{ padding: '0'}" shadow="hover">
+      <template #header>
+        <span>总计</span>
+      </template>
+           <el-menu >
+            <el-menu-item >
+              <template #title>
+                <el-icon style="color: #FFB500">
+                  <User/>
+                </el-icon>&nbsp;
+                <span>用户总数:</span>
+                <span style="margin-left: 60%">1人</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item >
+              <template #title>
+                <el-icon style="color: #FF3D00">
+                  <Document/>
+                </el-icon>&nbsp;
+                <span>订单总数:</span>
+                <span style="margin-left: 60%">1个</span>
+              </template>
+            </el-menu-item>
+          </el-menu>
+    </el-card>
+      </span>
 </template>
 
 <script>
+import {Document,User} from "@element-plus/icons";
+
 export default {
+  components: {
+    User,
+    Document
+  },
   mounted() {
     document.title = "站点统计"
     this.$axios.get("https://mc.rainspace.cn:4443/admin/count").then(res => {
@@ -57,5 +90,18 @@ export default {
 </script>
 
 <style scoped>
-
+.picture{
+  height: 70%;
+  width: 60%;
+  margin-top: 1%;
+  margin-left: 1%;
+  margin-right: 0.5%;
+  border: #E5E5E5 2px solid;
+  border-radius: 15px;
+}
+#number{
+  margin-top: 1%;
+  width: 23%;
+  z-index: -1;
+}
 </style>
