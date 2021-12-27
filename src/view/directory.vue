d6f9cf3eed8d9a9e3eb6b29dfaec19922c85f8b8
wangjustar <wangjustar@gmail.com>
2021-12-27 17:10:59 +0800

    增加选页数的数值

--- src/view/directory.vue
+++ src/view/directory.vue
@@ -77,6 +77,20 @@ export default {
       search: null,
       contacts: [],
       allContacts: [],
+      typeses: [{value: 1,},
+        {value: 2,},
+        {value: 3,},
+        {value: 4,},
+        {value: 5,},
+        {value: 6,},
+        {value: 7,},
+        {value: 8,},
+        {value: 9,},
+        {value: 10,},
+        {value: 11,},
+        {value: 12,},
+        {value: 13,},
+      ]
     }
   },
   watch: {
